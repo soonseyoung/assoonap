95aa70f61db66c5775c6b9a5fa12a777ec9d28e4
soonseyoung <soonseyoung06@gmail.com>
2025-03-16 11:30:27 +0900

    Add files via upload

---
+++ soon.py
@@ -0,0 +1,10 @@
+def get_radius(prompt):
+    r = int(input(prompt))
+    return r
+
+def get_circle_area(radius):
+    return 3.14*radius*radius
+
+radius = get_radius("넓이를 구하고자 하는 원의 반지름은? ")
+area = get_circle_area(radius)
+print("반지름", radius, "인 원의 넓이 = 3.14 *", radius, "*", radius, "=", area)
