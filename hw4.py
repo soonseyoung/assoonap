ea64103d7029e6496e4223133dacb0714cd1d08d
soonseyoung <soonseyoung06@gmail.com>
2025-04-10 23:33:54 +0900

    Add files via upload

---
+++ hw4.py
@@ -0,0 +1,19 @@
+def rep_char(c, n):
+    print(c * n)
+
+def draw_line_string(s):
+    rep_char('-', len(s))
+    print(s)
+    rep_char('-', len(s))
+
+name = input("Input his/her name: ")
+
+msg1 = "Hello " + name + ","
+msg2 = "Welcome to Seoul."
+
+nstr = len(msg1) if len(msg1) > len(msg2) else len(msg2)
+
+rep_char('-', nstr)
+print(msg1)
+print(msg2)
+rep_char('-', nstr)
