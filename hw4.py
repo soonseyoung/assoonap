5c084506fadb5fe28e9c355365f2affa430a7ff6
soonseyoung <soonseyoung06@gmail.com>
2025-04-10 23:48:08 +0900

    hw4.py

--- hw4.py
+++ hw4.py
@@ -1,14 +1,16 @@
 def rep_char(c, n):
     print(c * n)
 
+
 def draw_line_string(s):
     rep_char('-', len(s))
     print(s)
     rep_char('-', len(s))
 
+
 name = input("Input his/her name: ")
 
-msg1 = "Hello " + name + ","
+msg1 = f"Hello {name},"
 msg2 = "Welcome to Seoul."
 
 nstr = len(msg1) if len(msg1) > len(msg2) else len(msg2)
