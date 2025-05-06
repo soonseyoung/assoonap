e028c2c1bfc78556b4704f43c4f69e62f5ba9971
soonseyoung <soonseyoung06@gmail.com>
2025-05-06 18:43:04 +0900

    Add files via upload

--- hw7.py
+++ hw7.py
@@ -1,1 +1,12 @@
-print('s')
+shopping_bag={}
+
+print('구입')
+while True:
+    item=input('상품명? ')
+    if item=='':
+        break
+    amount=input('개수? ')
+    shopping_bag[item]=amount
+    print(f'장바구니에 {item}가(이) {amount}개 담겼습니다.')
+
+print(f'\n>>>장바구니 보기: {shopping_bag}')
